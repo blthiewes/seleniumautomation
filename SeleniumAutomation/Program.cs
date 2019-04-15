629d76994f0e7bf85eac7a4e0ceefcef7d28fe0a
Brent <blthiewes@gmail.com>
2019-04-14 22:11:13 -0500

    Initial commit

---
+++ SeleniumAutomation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SeleniumAutomationStandard/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SeleniumAutomationStandard
+{
+    public class Class1
+    {
+    }
+}
